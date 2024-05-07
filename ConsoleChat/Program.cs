7c75f5b85bcab32dfbd11a6184887e1439d003cd
xG0rynych <zmeigorynychllll@gmail.com>
2024-05-08 00:11:01 +0300

    Update Program.cs

--- ConsoleChat/Program.cs
+++ ConsoleChat/Program.cs
@@ -6,20 +6,12 @@ using System.Threading.Tasks;
 
 namespace ConsoleChat
 {
-    public class Ivan
-    {
-        public int A { get; set; }
-        public int B { get; set; }
-    }
-
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
             ChatManager chatManager = new ChatManager();
-            Console.WriteLine("ЗДАРОВА");
-
             chatManager.Start();
         }
     }
